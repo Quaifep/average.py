c3448614deaee903d444d61a74a0b3879d12e36c
Paul <quaifep@yahoo.com>
2021-01-12 09:00:40 -0800

    Project 2.a (average) push to GitHub

---
+++ average.py
@@ -0,0 +1,11 @@
+# Author: Paul Quaife
+# Date: 1/12/2020
+# Description: Asks for five numbers then averages them.
+
+print("Please enter five numbers.")
+num_1 = float(input())
+num_2 = float(input())
+num_3 = float(input())
+num_4 = float(input())
+num_5 = float(input())
+print("the average is", (num_1 + num_2 + num_3 + num_4 + num_5) / 5)  # inputs are averaged
