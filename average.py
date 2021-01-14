7a72715abe04cdd4e296fda58c7574880c3a3468
Quaifep <74254641+Quaifep@users.noreply.github.com>
2021-01-13 17:26:56 -0800

    Update average.py

--- average.py
+++ average.py
@@ -8,5 +8,5 @@ num_2 = float(input())
 num_3 = float(input())
 num_4 = float(input())
 num_5 = float(input())
-print("The average of those numbers is",)
+print("The average of those numbers is:",)
 print(float(num_1 + num_2 + num_3 + num_4 + num_5) / 5)  # inputs are averaged
